1bc705db6621f72e36c5f49223a19dcc536ec77c
hzcortex <iamwanghz@gmail.com>
2016-12-04 23:19:38 +0800

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -11,13 +11,14 @@ requests = requests.Session()
 requests.cookies = cookielib.LWPCookieJar('cookies')
 try:
     requests.cookies.load(ignore_discard=True)
-except:
+except Exception:
     pass
 
 
 def crawl():
     
-    # 使用 multiprocess 来进行多线程抓取
+    """ 使用 multiprocess 来进行多线程抓取
+    """
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36',
         'Host': 'www.lagou.com',
@@ -27,11 +28,9 @@ def crawl():
         'Accept-Language': 'zh-CN,zh;q=0.8',
         'X-Requested-With': 'XMLHttpRequest'
     }
-    r=requests.get('http://www.lagou.com/s/subscribe.html',headers=headers)
+    r = requests.get('http://www.lagou.com/s/subscribe.html',headers=headers)
     with open ('fuck.json','w+') as f:
         f.writelines(r.content)
-
-    # 关于 Cookies 的使用再看一遍 requests 的文档 yeah！！
 
 if __name__=='__main__':
     crawl()
